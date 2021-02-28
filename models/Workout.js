a51ce57da7db34a912546df672ce0ceecaecc3d3
Jessica D <deck.jessica@gmail.com>
2021-02-28 09:51:58 -0500

    set up models

---
+++ models/Workout.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const WorkoutSchema = new Schema ({
+    date: {
+        type: Date,
+        default: Date.now()
+    },
+
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true
+            },
+            name: {
+                type: String,
+                trim: true
+            },
+            duration: {
+                type: Number,
+                default: 0,
+                allowNull: true
+            },
+            distance: {
+                type: Number,
+                default: 0,
+                allowNull: true
+            },
+            weight: {
+                type: Number,
+                default: 0,
+                allowNull: true
+            },
+            sets: {
+                type: Number,
+                default: 0,
+                allowNull: true
+            },
+            reps: {
+                type: Number,
+                default: 0,
+                allowNull: true
+            }
+        }
+    ]
+
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+module.exports = Workout;
