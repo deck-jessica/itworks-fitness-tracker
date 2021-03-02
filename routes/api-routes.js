f6ac665e7938abaded65007c691565d589606635
Jessica D <deck.jessica@gmail.com>
2021-03-01 21:43:16 -0500

    set up api routes using api.js to see what the routes would hold

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,10 +2,20 @@ const db = require("../models");
 const router = require("express").Router();
 
 // create workout
-router.post("/api/workout", ({body}, res) => {
+router.post("/api/workouts", ({body}, res) => {
 
 });
 
+router.put("/api/workouts/:id", (req, res) => {
 
+});
+
+router.get("/api/workouts", (req, res) => {
+
+});
+
+router.get("/api/workouts/range", (req, res) => {
+
+});
 
 module.exports = router;
