35d140d4f2f9e6e668cb552b6ced3283870c95c7
Jessica D <deck.jessica@gmail.com>
2021-03-01 20:37:09 -0500

    deploying to Heroku

---
+++ routes/api-routes.js
@@ -0,0 +1,11 @@
+const db = require("../models");
+const router = require("express").Router();
+
+// create workout
+router.post("/api/workout", ({body}, res) => {
+
+});
+
+
+
+module.exports = router;
