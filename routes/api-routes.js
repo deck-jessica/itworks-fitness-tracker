7525524d4a410c3b7a75e9d28af64aa4c7e12e71
Jessica D <deck.jessica@gmail.com>
2021-03-01 23:08:49 -0500

    added put route to routes, will put new workout in db

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -14,6 +14,17 @@ router.post("/api/workouts", ({body}, res) => {
 });
 
 router.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findOneAndUpdate(
+        { _id: req.params.id},
+    { $push: { exercises: req.body}},
+    { new: true})
+    .then((dbWorkout) => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.json(err);
+        console.log(err);
+    });
 
 });
 
